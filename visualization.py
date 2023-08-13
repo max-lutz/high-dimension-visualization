5236b83e9845532a61ce6cf8311c901dd97ed124
max_lutz <max.lutz.09@gmail.com>
2023-08-13 19:35:11 +0200

    fix error when user hasn't selected dataset to upload

--- visualization.py
+++ visualization.py
@@ -92,6 +92,7 @@ def split_columns(df, drop_cols=[]):
 
 
 def load_dataset(dataset):
+    df = None
     if (dataset == 'Load my own dataset'):
         uploaded_file = st.file_uploader('File uploader')
         if uploaded_file is not None:
@@ -141,111 +142,112 @@ dataset = st.selectbox('Select dataset', ['Titanic dataset', 'Heart disease data
                                           'Wine dataset', 'Load my own dataset'])
 df = load_dataset(dataset)
 
-st.sidebar.header('Select feature to predict')
-_, cat_cols, _, _, _ = split_columns(df)
-target_list = [x for x in df.columns.to_list() if x in cat_cols]
-target_list.reverse()
-target_selected = st.sidebar.selectbox('Predict', target_list)
+if (df is not None):
+    st.sidebar.header('Select feature to predict')
+    _, cat_cols, _, _, _ = split_columns(df)
+    target_list = [x for x in df.columns.to_list() if x in cat_cols]
+    target_list.reverse()
+    target_selected = st.sidebar.selectbox('Predict', target_list)
 
-X = df.drop(columns=target_selected)
-Y = df[target_selected].values.ravel()
+    X = df.drop(columns=target_selected)
+    Y = df[target_selected].values.ravel()
 
-drop_cols = []
-num_cols, cat_cols, text_cols, num_cols_missing, cat_cols_missing = split_columns(X, drop_cols)
+    drop_cols = []
+    num_cols, cat_cols, text_cols, num_cols_missing, cat_cols_missing = split_columns(X, drop_cols)
 
-# create new lists for columns with missing elements
-for col in X.columns:
-    if (col in num_cols and X[col].isna().sum() > 0):
-        num_cols.remove(col)
-        num_cols_missing.append(col)
-    if (col in cat_cols and X[col].isna().sum() > 0):
-        cat_cols.remove(col)
-        cat_cols_missing.append(col)
+    # create new lists for columns with missing elements
+    for col in X.columns:
+        if (col in num_cols and X[col].isna().sum() > 0):
+            num_cols.remove(col)
+            num_cols_missing.append(col)
+        if (col in cat_cols and X[col].isna().sum() > 0):
+            cat_cols.remove(col)
+            cat_cols_missing.append(col)
 
-# combine text columns in one new column because countVectorizer does not accept multiple columns
-text_cols_original = text_cols
-if (len(text_cols) != 0):
-    X['text'] = X[text_cols].astype(str).agg(' '.join, axis=1)
-    for cols in text_cols:
-        drop_cols.append(cols)
-    text_cols = "text"
+    # combine text columns in one new column because countVectorizer does not accept multiple columns
+    text_cols_original = text_cols
+    if (len(text_cols) != 0):
+        X['text'] = X[text_cols].astype(str).agg(' '.join, axis=1)
+        for cols in text_cols:
+            drop_cols.append(cols)
+        text_cols = "text"
 
+    missing_cat_pipeline = make_pipeline(SimpleImputer(strategy='most_frequent', missing_values=np.nan))
+    missing_cat_pipeline.steps.append(('encoding', OneHotEncoder(handle_unknown='ignore')))
 
-missing_cat_pipeline = make_pipeline(SimpleImputer(strategy='most_frequent', missing_values=np.nan))
-missing_cat_pipeline.steps.append(('encoding', OneHotEncoder(handle_unknown='ignore')))
+    missing_num_pipeline = make_pipeline(SimpleImputer(strategy='median', missing_values=np.nan))
+    missing_num_pipeline.steps.append(('scaling', StandardScaler()))
 
-missing_num_pipeline = make_pipeline(SimpleImputer(strategy='median', missing_values=np.nan))
-missing_num_pipeline.steps.append(('scaling', StandardScaler()))
+    # need to make two preprocessing pipeline too handle the case encoding without imputer...
+    preprocessing = make_column_transformer(
+        (missing_cat_pipeline, cat_cols_missing),
+        (missing_num_pipeline, num_cols_missing),
 
-# need to make two preprocessing pipeline too handle the case encoding without imputer...
-preprocessing = make_column_transformer(
-    (missing_cat_pipeline, cat_cols_missing),
-    (missing_num_pipeline, num_cols_missing),
+        (OneHotEncoder(handle_unknown='ignore'), cat_cols),
+        (CountVectorizer(), text_cols),
+        (StandardScaler(), num_cols)
+    )
 
-    (OneHotEncoder(handle_unknown='ignore'), cat_cols),
-    (CountVectorizer(), text_cols),
-    (StandardScaler(), num_cols)
-)
+    st.header('Preprocessing')
+    X_preprocessed = preprocessing.fit_transform(X)
+    row1_spacer1, row1_1, row1_spacer2, row1_2, row1_spacer3 = st.columns((SPACER/10, ROW, SPACER, ROW, SPACER/10))
+    with row1_1:
+        st.header('Original dataset')
+        st.text(f'Original dataframe has shape: {X.shape}')
+        st.write(df)
 
-st.header('Preprocessing')
-X_preprocessed = preprocessing.fit_transform(X)
-row1_spacer1, row1_1, row1_spacer2, row1_2, row1_spacer3 = st.columns((SPACER/10, ROW, SPACER, ROW, SPACER/10))
-with row1_1:
-    st.header('Original dataset')
-    st.text(f'Original dataframe has shape: {X.shape}')
-    st.write(df)
+    with row1_2:
 
-with row1_2:
+        st.header('Preprocessed dataset')
+        display_dataframe = True
+        try:
+            pd.DataFrame(X_preprocessed)
+        except:
+            display_dataframe = False
+        if (X_preprocessed.shape[1] < 100 and display_dataframe):
+            st.text(f'Processed dataframe has shape: {X_preprocessed.shape}')
+            st.write(X_preprocessed)
+        else:
+            st.text(f'Processed dataframe cannot be displayed, shape: {X_preprocessed.shape}')
 
-    st.header('Preprocessed dataset')
-    display_dataframe = True
-    try:
-        pd.DataFrame(X_preprocessed)
-    except:
-        display_dataframe = False
-    if (X_preprocessed.shape[1] < 100 and display_dataframe):
-        st.text(f'Processed dataframe has shape: {X_preprocessed.shape}')
-        st.write(X_preprocessed)
+    dim = preprocessing.fit_transform(X).shape[1]
+    if (dim > 2):
+        st.sidebar.title('Dimension reduction')
+        dimension_reduction_algorithm = st.sidebar.selectbox('Algorithm', ['Kernel PCA', 'Truncated SVD'])
+
+        hyperparameters_dim_reduc = {}
+        hyperparameters_dim_reduc['n_components'] = 2
+        if (dimension_reduction_algorithm == 'Kernel PCA'):
+            hyperparameters_dim_reduc['kernel'] = st.sidebar.selectbox(
+                'Kernel (default = linear)', ['linear', 'poly', 'rbf', 'sigmoid', 'cosine'])
+        if (dimension_reduction_algorithm == 'Truncated SVD'):
+            hyperparameters_dim_reduc['solver'] = st.sidebar.selectbox(
+                'Solver (default = randomized)', ['randomized', 'arpack'])
     else:
-        st.text(f'Processed dataframe cannot be displayed, shape: {X_preprocessed.shape}')
+        st.sidebar.title('Dimension reduction')
+        dimension_reduction_algorithm = st.sidebar.selectbox('Number of features too low', ['None'])
+        hyperparameters_dim_reduc = {}
 
+    dimension_reduction_pipeline = Pipeline([
+        ('preprocessing', preprocessing),
+        ('dimension reduction', get_dim_reduc_algo(dimension_reduction_algorithm, hyperparameters_dim_reduc))
+    ])
 
-dim = preprocessing.fit_transform(X).shape[1]
-if (dim > 2):
-    st.sidebar.title('Dimension reduction')
-    dimension_reduction_algorithm = st.sidebar.selectbox('Algorithm', ['Kernel PCA', 'Truncated SVD'])
+    dimension_reduction_pipeline.fit(X, Y)
+    X_reduced = pd.DataFrame(dimension_reduction_pipeline.transform(X))
+    X_reduced.columns = [f"component {i+1}" for i in range(len(X_reduced.columns))]
+    X_reduced['target'] = Y
 
-    hyperparameters_dim_reduc = {}
-    hyperparameters_dim_reduc['n_components'] = 2
-    if (dimension_reduction_algorithm == 'Kernel PCA'):
-        hyperparameters_dim_reduc['kernel'] = st.sidebar.selectbox(
-            'Kernel (default = linear)', ['linear', 'poly', 'rbf', 'sigmoid', 'cosine'])
-    if (dimension_reduction_algorithm == 'Truncated SVD'):
-        hyperparameters_dim_reduc['solver'] = st.sidebar.selectbox(
-            'Solver (default = randomized)', ['randomized', 'arpack'])
+    if (len(X_reduced.columns) == 3):
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.set_title('Dataset with reduced dimensions')
+        sns.scatterplot(x='component 1', y='component 2', data=X_reduced, hue='target', ax=ax)
+        st.pyplot(fig)
+
+    # if (len(X_reduced.columns) == 4):
+    #     fig = px.scatter_3d(X_reduced, x='component 1', y='component 2', z='component 3',
+    #                         symbol='target', color='target', width=800, height=400)
+    #     st.plotly_chart(fig, use_container_width=True)
+
 else:
-    st.sidebar.title('Dimension reduction')
-    dimension_reduction_algorithm = st.sidebar.selectbox('Number of features too low', ['None'])
-    hyperparameters_dim_reduc = {}
-
-dimension_reduction_pipeline = Pipeline([
-    ('preprocessing', preprocessing),
-    ('dimension reduction', get_dim_reduc_algo(dimension_reduction_algorithm, hyperparameters_dim_reduc))
-])
-
-dimension_reduction_pipeline.fit(X, Y)
-X_reduced = pd.DataFrame(dimension_reduction_pipeline.transform(X))
-X_reduced.columns = [f"component {i+1}" for i in range(len(X_reduced.columns))]
-X_reduced['target'] = Y
-
-
-if (len(X_reduced.columns) == 3):
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.set_title('Dataset with reduced dimensions')
-    sns.scatterplot(x='component 1', y='component 2', data=X_reduced, hue='target', ax=ax)
-    st.pyplot(fig)
-
-# if (len(X_reduced.columns) == 4):
-#     fig = px.scatter_3d(X_reduced, x='component 1', y='component 2', z='component 3',
-#                         symbol='target', color='target', width=800, height=400)
-#     st.plotly_chart(fig, use_container_width=True)
+    st.sidebar.header('')
